e9c7cb89bf7cb0f33bb3ea9529651f41eb112bb1
Khaled Sliman <Khaled.Ah.Sliman@gmail.com>
2025-09-12 19:40:11 -0500

    Mark down

--- export-api-container.ps1
+++ export-api-container.ps1
@@ -1,2 +1,2 @@
-docker build --no-cache -t agentic-rag-api:v1.1.1.16 -f fastapi/Dockerfile fastapi
-docker save agentic-rag-api:v1.1.1.16 -o agentic-rag-api_v1.1.1.16.tar
+docker build --no-cache -t agentic-rag-api:v1.1.1.17 -f fastapi/Dockerfile fastapi
+docker save agentic-rag-api:v1.1.1.17 -o agentic-rag-api_v1.1.1.17.tar
